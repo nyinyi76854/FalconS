7ce019ca175bad51886191a54b9ff8cf328da6a3
nyinyi76854 <148785112+nyinyi76854@users.noreply.github.com>
2025-02-02 09:51:21 +0630

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        icon: path.join(__dirname, 'assets', 'echo.png'), 
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            contextIsolation: true,
+            enableRemoteModule: false
+        }
+    });
+
+    // Load your initial HTML file
+    mainWindow.loadFile('https://nyinyi76854.github.io/FalconS/index.html'); 
+
+    // Hide the menu bar
+    mainWindow.setMenuBarVisibility(false);
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
